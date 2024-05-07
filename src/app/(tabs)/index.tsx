5a5fc10afd7b8c79619adbd5952995198f220788
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-05-07 10:58:02 +0100

    fixed bundling issues

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,17 +4,7 @@ import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello world!</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({

de6e7814bf525d1dd354a59cc986571f8bd31805
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-05-08 10:51:28 +0100

    chore: Update product images and styles in MenuScreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,23 @@
 import ProductListItem from "@/components/ProductListItem";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import Products from "@assets/data/products";
 
 export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <ProductListItem product={Products[0]} />
-      <ProductListItem product={Products[1]} />
-    </View>
+    <FlatList
+      data={Products}
+      numColumns={2}
+      contentContainerStyle={styles.container}
+      columnWrapperStyle={{ gap: 10 }}
+      bounces={true}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
+    gap: 10,
     padding: 10,
-    borderRadius: 20,
   },
 });

7583f1efd6f4739359031fb0a3f5ffe7f8d97798
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-07-11 10:58:49 +0100

    added fetch

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,8 +1,27 @@
+import { supabase } from "@/app/lib/supabase";
 import ProductListItem from "@/components/ProductListItem";
 import Products from "@assets/data/products";
+import { useEffect } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 export default function MenuScreen() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data, error } = await supabase
+        .from("products")
+        .select("*")
+        .order("name", { ascending: true });
+      if (error) {
+        console.log("error", error);
+      }
+      if (data) {
+        console.log(data);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <FlatList
       data={Products}
